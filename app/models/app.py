c396e6fe99a2790a4b68488f059a957a8920ff8b
PetrusCode <peterkayode618@gmail.com>
2025-02-06 12:09:25 +0100

    create an index function

--- app/models/app.py
+++ app/models/app.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 from app.api.v1.endpoints import auth, blog
 from app.core.config import settings
 
-app = FastAPI(title="AI-Powered Blog Post Creation API")
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {"message": "AI-Powered Blog Post Creation API"}
 
 app.include_router(auth.router, prefix="/api/v1")
 app.include_router(blog.router, prefix="/api/v1")
